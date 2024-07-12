8f66ba547d306d3c12ed8819e7e6d1d4f774269f
Andrew Motko <andrew.motko@gmail.com>
2024-07-13 02:45:03 +0300

    fix: extention sort add

--- task_01.py
+++ task_01.py
@@ -13,9 +13,10 @@ def recursive_copy(src: Path, dst: Path):
     try:
         for item in src.iterdir():
             if item.is_dir():
-                recursive_copy(item, dst)
+                recursive_copy(item, dst / item.name)
             else:
-                folder = dst / item.name[:1]  
+                file_extension = item.suffix.lower()[1:]
+                folder = dst / file_extension
                 folder.mkdir(parents=True, exist_ok=True)
                 shutil.copy2(item, folder)
     except (FileNotFoundError, PermissionError) as e:
@@ -30,26 +31,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-# # ANSI escape codes for colored output
-# COLOR_BLUE = "\033[94m"
-# COLOR_RESET = "\033[0m"
 
-# def display_tree(path: Path, indent: str = "", prefix: str = "") -> None:
-#     if path.is_dir():
-#         # Use blue color for directories
-#         print(indent + prefix + COLOR_BLUE + str(path.name) + COLOR_RESET)
-#         indent += "    " if prefix else ""
-
-#         # Get a sorted list of children, with directories last
-#         children = sorted(path.iterdir(), key=lambda x: (x.is_file(), x.name))
-
-#         for index, child in enumerate(children):
-#             # Check if the current child is the last one in the directory
-#             is_last = index == len(children) - 1
-#             display_tree(child, indent, "└── " if is_last else "├── ")
-#     else:
-#         print(indent + prefix + str(path.name))
-
-# if __name__ == "__main__":
-#     root = Path("picture")
-#     display_tree(root)
